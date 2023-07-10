64e2450e05729b4898c3ef779ebf4b58204022f8
Andrew Pipo <atpipo2026@gmail.com>
2023-07-10 18:52:25 -0400

    Added method of deleting item from list via specified user input

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,8 @@ public class Main {
                 case "A": // if user enters A or a, it will allow user to add to list or array
                     addItem();
                     break;
-                case "D":
-                 //   deleteItem();
+                case "D": // if user enters D or d, it will allow user to delete item from list or array
+                    deleteItem();
                     break;
                 case "P": // if user enters P or p, it will print out list/array
                     printList();
@@ -30,17 +30,18 @@ public class Main {
                 case "Q": // if user enters Q or q, it will ask to confirm their exit
                     exitProgram = confirmExit();
                     break;
-                default:
+                default: // default case in event user input is invalid and safe input methods do not catch invalid inputs as intended
                     System.out.println("Invalid choice. Please try again.");
             }
         }
     }
 
     private static void displayMenu() {
+        // print out the menu options for user to select from
         System.out.println("Menu:");
-        System.out.println("A - Add an item to the list");
-        System.out.println("D - Delete an item from the list");
-        System.out.println("P - Print out the list");
+        System.out.println("A - Add an item to the array/list");
+        System.out.println("D - Delete an item from the array/list");
+        System.out.println("P - Print out the array/list");
         System.out.println("Q - Quit/Exit the program");
     }
 
@@ -49,6 +50,19 @@ public class Main {
         String item = SafeInput.getNonZeroLenString(in, "Enter an item to add");
         menuArrayList.add(item);
         System.out.println("Item added: " + item);
+    }
+
+    // method to delete item from array/list
+    private static void deleteItem() {
+        if (menuArrayList.isEmpty()) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        System.out.println("Current list:");
+        displayNumberedItems();
+        int index = SafeInput.getRangedInt(in,"Enter the number of the item to delete: ", 1, menuArrayList.size());
+        String item = menuArrayList.remove(index - 1);
+        System.out.println("Item deleted: " + item);
     }
 
 
@@ -64,6 +78,7 @@ public class Main {
         return SafeInput.getYNConfirm(in,"Are you sure you want to quit? (Y/N): ");
     }
 
+    // method to display the array as number list
     private static void displayNumberedItems() {
         // iterates through array and numbers the elements and prints out a numbered list of elements
         for (int i = 0; i < menuArrayList.size(); i++) {
